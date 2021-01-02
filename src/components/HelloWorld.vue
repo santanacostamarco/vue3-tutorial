1e3ee8e323b68c4850e213ed067ae4ea516795dd
Marco Santana <santannacostamarco@gmail.com>
2021-01-02 17:42:13 -0300

    initialized vue 3 app with vite

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <h3>This is f*cking AWESOME</h3>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
